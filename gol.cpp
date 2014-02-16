c380458db33868e7800d652932fc616cbb66b130
Ruwen Hahn <rhahn@stunned.de>
2014-02-16 20:53:23 +0100

    Various stuff
    - Wider state chart
    - Faster hashing
    - Less warnings
    - glDrawPixels instead of texture

--- gol.cpp
+++ gol.cpp
@@ -8,23 +8,25 @@
 #include <chrono>
 #include <limits>
 #include <cmath>
+#include <cstdint>
 
 using namespace std;
 
-const unsigned Width = 800,
-               Height = 600;
+const unsigned Width = 1280,
+               Height = 960;
 
 const string Title = "Game of Life";
 
 typedef unsigned char uchar;
-typedef uchar state_chart_t[2][2][2][2][2][2][2][2][2];
+typedef uint16_t state_chart_t[2][2][2][2][2][2][2][2][2][2][2][2];
 typedef uchar world_t[Height][Width];
 
 void init_scene(GLuint&);
 void destroy_scene(GLuint&);
-void draw_scene(GLuint&);
 template <typename T>
-void update_texture(GLuint&, T);
+void draw_scene(GLuint&, const T&);
+template <typename T>
+void update_texture(GLuint&, const T&);
 
 void create_state_chart(state_chart_t);
 
@@ -32,22 +34,27 @@ void create_world(world_t);
 void update_world(world_t /*cur*/, world_t/*prev*/, state_chart_t);
 
 namespace detail {
-    struct hash_fnv
+    size_t hash_fnv(const world_t& world)
     {
-    public:
-        size_t operator()(const world_t& world) const
-        {
-            size_t res = numeric_limits<size_t>::digits == 64 ? 14695981039346656037UL : 2166136261UL;
-            auto const prime = numeric_limits<size_t>::digits == 64 ? 1099511628211UL : 16777619UL;
-            for(auto y = 0; y < Height; y++)
-                for(auto x = 0; x < Width; x++)
-                {
-                    res ^= world[y][x];
-                    res *= prime;
-                }
-            return res;
-        }
-    };
+        size_t res = numeric_limits<size_t>::digits == 64 ? 14695981039346656037UL : 2166136261UL;
+        auto const prime = numeric_limits<size_t>::digits == 64 ? 1099511628211UL : 16777619UL;
+        static_assert(Width%8 == 0, "must be multiple of 8");
+        for(auto y = 0; y < Height; y++)
+            for(auto x = 0; x < Width; x += 8)
+            {
+                uint8_t octet = (world[y][x] & 0x80) |
+                    (world[y][x+1] & 0x40) |
+                    (world[y][x+2] & 0x20) |
+                    (world[y][x+3] & 0x10) |
+                    (world[y][x+4] & 0x08) |
+                    (world[y][x+5] & 0x04) |
+                    (world[y][x+6] & 0x02) |
+                    (world[y][x+7] & 0x01);
+                res ^= octet;
+                res *= prime;
+            }
+        return res;
+    }
 }
 
 struct timer
@@ -58,9 +65,8 @@ struct timer
     bool expired() { return chrono::steady_clock::now() >= end; }
     void reset(string header)
     {
-        return;
         auto now = chrono::steady_clock::now();
-        cout << header << ' ' << chrono::duration_cast<chrono::milliseconds>(now - start).count() << " ms\n";
+        cout << header << ' ' << chrono::duration_cast<chrono::microseconds>(now - start).count() << " us\n";
         start = now;
     }
 };
@@ -99,7 +105,7 @@ int main()
     unordered_map<size_t, unsigned long> history;
     bool cyclic = false;
     vector<vector<uchar>> cycle;
-    unsigned cycle_length;
+    unsigned cycle_length = 0;
     unsigned cycles_initialized = 0;
     timer reset_timer;
     while(glfwGetWindowParam(GLFW_OPENED))
@@ -129,7 +135,7 @@ int main()
                 update_world(world[updates%2], world[(updates-1)%2], state_chart);
         }
         t.reset("update world");
-        size_t hash = ::detail::hash_fnv()(world[updates%2]);
+        size_t hash = ::detail::hash_fnv(world[updates%2]);
         t.reset("hash");
         auto pos = history.find(hash);
         if(pos == history.end())
@@ -144,6 +150,7 @@ int main()
                 cycle.resize(cycle_length);
                 cycles_initialized = 0;
                 reset_timer = timer(sqrt(cycle_length));
+                return 0;
             }
         }
         t.reset("foo");
@@ -152,7 +159,7 @@ int main()
         else
             update_texture(texture, world[updates%2]);
         t.reset("texture");
-        draw_scene(texture);
+        draw_scene(texture, world);
         t.reset("draw");
         glfwSwapBuffers();
         t.reset("swap");
@@ -161,25 +168,29 @@ int main()
 
 void create_state_chart(state_chart_t chart)
 {
+    /*  1   2   3   4
+     *  5   l   r   6
+     *  7   8   9   10
+     */
     union
     {
         unsigned val;
         struct { unsigned b0:1,b1:1,b2:1,b3:1,b4:1,b5:1,b6:1,b7:1,b8:1,b9:1,b10:1,b11:1,b12:1; } b1;
     } i;
-    for(i.val = 0; i.val < (1<<9); i.val++)
+    for(i.val = 0; i.val < (1<<12); i.val++)
     {
-        auto sum = [&]() -> int {
-            auto j = i.val - ((i.val>>1)&0x55555555);
+        const auto sum = [](unsigned i) -> unsigned {
+            auto j = i - ((i>>1)&0x55555555);
             j = (j & 0x33333333) + ((j >> 2) & 0x33333333);
             return (((j + (j >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
-        }() - i.b1.b8;
+        };
+        const auto l = [](unsigned s, unsigned l) -> uchar {
+            return l ? ((s == 2 || s == 3) ? 255 : 0) : (s == 3 ? 255 : 0);
+        };
         //fmt.Printf("%v -%v-> %v\n", i, (i>>8)&1, sum)
-        if(i.b1.b8 == 1)
-            chart[i.b1.b8][i.b1.b7][i.b1.b6][i.b1.b5][i.b1.b4][i.b1.b3][i.b1.b2][i.b1.b1][i.b1.b0] =
-                (sum == 2 || sum == 3) ?  255 : 0;
-        else
-            chart[i.b1.b8][i.b1.b7][i.b1.b6][i.b1.b5][i.b1.b4][i.b1.b3][i.b1.b2][i.b1.b1][i.b1.b0] =
-                sum == 3 ? 255 : 0;
+        const auto left_mask = 0x757,
+                   right_mask = 0xeae;
+        chart[i.b1.b11][i.b1.b10][i.b1.b9][i.b1.b8][i.b1.b7][i.b1.b6][i.b1.b5][i.b1.b4][i.b1.b3][i.b1.b2][i.b1.b1][i.b1.b0] = l(sum(i.val&right_mask), i.b1.b6) | (l(sum(i.val&left_mask), i.b1.b5)<<8);
     }
 }
 
@@ -195,35 +206,36 @@ void create_world(world_t world)
 
 void update_world(world_t cur, world_t prev, state_chart_t chart)
 {
-    using namespace std::chrono;
-    auto start = steady_clock::now();
     for(auto y = 0; y < Height; y++)
-        for(auto x = 0; x < Width; x++)
+        for(auto x = 0; x < Width; x+=2)
         {
-            const auto hy = Height+y;
-            const auto wx = Width+x;
-            cur[y][x] = chart[prev[y][x]/255][prev[(hy-1)%Height][(wx-1)%Width]/255][prev[(hy-1)%Height][x]/255][prev[(hy-1)%Height][(wx+1)%Width]/255][prev[y][(wx-1)%Width]/255][prev[y][(wx+1)%Width]/255][prev[(hy+1)%Height][(wx-1)%Width]/255][prev[(hy+1)%Height][x]/255][prev[(hy+1)%Height][(wx+1)%Width]/255];
+            const auto h = [&](int h) -> unsigned {
+                return (Height+y+h)%Height;
+            };
+            const auto w = [&](int w) -> unsigned {
+                return (Width+x+w)%Width;
+            };
+            *((uint16_t*)&cur[y][x]) = chart[prev[h(-1)][w(-1)]/255][prev[h(-1)][x]/255][prev[h(-1)][w(1)]/255][prev[h(-1)][w(2)]/255][prev[y][w(-1)]/255][prev[y][x]/255][prev[y][w(1)]/255][prev[y][w(2)]/255][prev[h(1)][w(-1)]/255][prev[h(1)][x]/255][prev[h(1)][w(1)]/255][prev[h(1)][w(2)]/255];
         }
-    //cout << duration_cast<milliseconds>(steady_clock::now() - start).count() << " ms\n";
 }
 
 template <typename T>
-void update_texture(GLuint& tex, T world)
+void update_texture(GLuint& tex, const T& world)
 {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, Width, Height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, world);
+    //glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, Width, Height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, world);
 }
 
 void init_texture(GLuint& tex)
 {
-    glGenTextures(1, &tex);
+    /*glGenTextures(1, &tex);
     glBindTexture(GL_TEXTURE_2D, tex);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);*/
 }
 
 void init_scene(GLuint& tex)
 {
-    glEnable(GL_TEXTURE_2D);
+    //glEnable(GL_TEXTURE_2D);
     glClearColor(1, 1, 1, 1);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -236,15 +248,17 @@ void init_scene(GLuint& tex)
 
 void destroy_scene(GLuint& tex)
 {
-    glDeleteTextures(1, &tex);
+    //glDeleteTextures(1, &tex);
 }
 
-void draw_scene(GLuint& tex)
+template <typename T>
+void draw_scene(GLuint& tex, const T& world)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glBindTexture(GL_TEXTURE_2D, tex);
+    glDrawPixels(Width, Height, GL_LUMINANCE, GL_UNSIGNED_BYTE, world);
+    /*glBindTexture(GL_TEXTURE_2D, tex);
     glColor4f(1, 1, 1, 1);
 
     glBegin(GL_QUADS);
@@ -256,7 +270,7 @@ void draw_scene(GLuint& tex)
     glVertex2f(Width, Height);
     glTexCoord2f(1, 0);
     glVertex3f(Width, 1, 0);
-    glEnd();
+    glEnd();*/
 
 }
 
